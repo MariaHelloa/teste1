3014041376d8fa3d018afc1af7f0154f6a259537
MariaHelloa <115737038+MariaHelloa@users.noreply.github.com>
2023-01-16 14:16:06 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-import jsonServer from 'json-server';
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200;
-server.use(middlewares);
-server.use(router);
-server.listen(port);
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
+
